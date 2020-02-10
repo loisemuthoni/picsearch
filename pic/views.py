1af402c9a8f2f7e1fca303f203e8c52a90da68bb
Moringa School <loisemburu01@gmail.com>
2020-02-10 11:28:39 +0300

    added template files

--- pic/views.py
+++ pic/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def register(request):
