5f5466f0550bb94dac38215aa743c35670ead4c3
Moringa School <loisemburu01@gmail.com>
2020-02-10 11:03:38 +0300

    created a register instance

--- app/urls.py
+++ app/urls.py
@@ -18,4 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('register/',user_views.register, name = 'register'),
 ]
--- pic/views.py
+++ pic/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = InstaRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data['email']
+            fullName = form.cleaned_data['fullName']
+            username = form.cleaned_data.get('username')
+            password1 = form.cleaned_data['password1']
+            password2 = form.cleaned_data['password2']
+            messages.success(request, f'Account created for {username}')
+            return redirect('home')
+    else:
+        form = InstaRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
