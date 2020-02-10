20e00b47f5e4f8dfdec88808f80157fedbede783
Moringa School <loisemburu01@gmail.com>
2020-02-10 11:49:02 +0300

    modified content in templates

--- app/urls.py
+++ app/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+# from django.conf.urls import url
 from django.contrib import admin
-from django.urls import url,include
+from django.urls import path,include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url('register/',user_views.register, name = 'register'),
-    url('',include('app.urls')),
+    path(r'^admin/', admin.site.urls),
+    path('register/',user_views.register, name = 'register'),
+    path('',include('app.urls')),
 ]
