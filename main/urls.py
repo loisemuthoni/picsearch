479f730b16670f685c8c4c7dc90393846dedf207
Moringa School <loisemburu01@gmail.com>
2020-02-11 13:45:33 +0300

    corrected the urls

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-from django.urls import re_path,path
+from django.urls import url,path
 from .views import (
     # ImageListView,
     ImageDetailView,
@@ -14,13 +14,13 @@ from .views import (
 
 
 urlpatterns = [
-    re_path(r'^$',Index_View,name="index_view"),
-    re_path(r'^image/(?P<pk>\d+)$',ImageDetailView.as_view(),name="image_detail"),
-    re_path(r'^image/new$',ImageCreateView.as_view(),name="image_upload"),
-    re_path(r'^image/(?P<pk>\d+)/update',ImageUpdateView.as_view(),name="image_update"),
-    re_path(r'^image/(?P<pk>\d+)/delete',ImageDeleteView.as_view(),name="image_delete"),
-    re_path(r'^user/(?P<pk>\d+)',OtherProfile,name="single_profile"),
-    re_path(r'^search/$',ImageSearch,name="search-results"),
-    re_path(r'^comment/(?P<pk>\d+)',CommentOnImage,name="image-comment"),
-    re_path(r'^like/(?P<pk>\d+)',Like,name="image-like"),
+    url(r'^$',Index_View,name="index_view"),
+    url(r'^image/(?P<pk>\d+)$',ImageDetailView.as_view(),name="image_detail"),
+    url(r'^image/new$',ImageCreateView.as_view(),name="image_upload"),
+    url(r'^image/(?P<pk>\d+)/update',ImageUpdateView.as_view(),name="image_update"),
+    url(r'^image/(?P<pk>\d+)/delete',ImageDeleteView.as_view(),name="image_delete"),
+    url(r'^user/(?P<pk>\d+)',OtherProfile,name="single_profile"),
+    url(r'^search/$',ImageSearch,name="search-results"),
+    url(r'^comment/(?P<pk>\d+)',CommentOnImage,name="image-comment"),
+    url(r'^like/(?P<pk>\d+)',Like,name="image-like"),
 ]
