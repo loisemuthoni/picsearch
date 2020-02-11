5b98fce0c682af205b487925353327bf0ab88ebf
Moringa School <loisemburu01@gmail.com>
2020-02-11 16:30:57 +0300

    added production in settings

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -95,6 +95,14 @@ if config('MODE')=="dev":
        
    }
 
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }   
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
